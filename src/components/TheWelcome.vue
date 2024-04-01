7a516517de7383a5496656a07f6cba00452e6139
nurasyifah <nurasyifah39@gmail.com>
2024-04-01 10:20:53 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,89 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Educational Background</template>
+
+    (2009-2010)-
+    <a href="https://referensi.data.kemdikbud.go.id/pendidikan/npsn/69994681" target="_blank" rel="noopener">TK Raudhatul Athfal Al Hidayah</a>, 
+    (2010-2016)-
+    <a href="https://referensi.data.kemdikbud.go.id/pendidikan/npsn/69819451" target="_blank" rel="noopener">SD Madrasah Ibtidaiyah Sa'adah El Islamiyah</a>, 
+    (2016-2019)-
+    <a href="https://sekolah.data.kemdikbud.go.id/index.php/Chome/profil/E0D44DFF-F8FB-4E3B-80D7-02277442DE67" target="_blank" rel="noopener">SMP Madrasah Tsanawiyah Sa'adah El Islamiyah</a>, 
+     (2019-2022)-
+    <a href="https://smkn1tbh.sch.id/home/halaman/59" target="_blank" rel="noopener">SMK Teknik Komputer & Jaringan- SMK Negeri 1 Tembilahan Kota</a>, 
+    (2022-Sekarang)-
+    <a href="https://uir.ac.id/" target="_blank" rel="noopener">S1 Teknik Informatika- Universitas Islam Riau</a>.   
+    <a href="https://vuejs.org/" target="_blank" rel="noopener"></a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Work Experience</template>
+
+    (2021/2022) Praktik Kerja Lapangan (PKL) on
+    <a href="https://imigrasitembilahan.com/" target="_blank" rel="noopener">Office Immigration Class II TPI Tembilahan</a>.
+    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener"></a>
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener"></a>
+    <a href="https://www.cypress.io/" target="_blank" rel="noopener"></a>
+    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
+      ></a
+    >
+    <br />
+    <code></code>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Certificate</template>
+
+    Certificate Of Competence :
+    Indonesian National Qualification Framework Level II on Business Skills Competence Computer and Network Engineering and
+    On behalf of BNSP P-1 State Vocational High School 1 Tembilahan Professional Certificaton Institute
+    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener"></a>
+    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener"></a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Contact Me</template>
+
+    My Number 
+    +62852-5609-1271 Email 
+    
+      nurasyifah39@gmail.com
+      and My Instagram
+    <a href="https://www.instagram.com/nur__asyifah?igsh=MTV1ODk0cno5c3huNQ==" target="_blank" rel="noopener">@nur__asyifah</a>
+    <a href="https://www.instagram.com/cwippp" target="_blank" rel="noopener">@cwippp</a>.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>About Me</template>
+
+    hello I'm cipa, my family calls me that I'm the 3rd child of 3 siblings yes I'm the last child but I'm not spoiled ok
+    :)
+    I'm was born on May 11, 2004 yes now I'm 19 years old my zodiac sign is Taurus (the stubborn bull) but it's okay
+    then my hobby is reading anything because I like learning then my favorite food is chocolate "Dairy Milk" my favorite drink is "Taro" . 
+    The last is my quote that always remember is "if you never do, you'll never know" and "learning is a process to become better than before" .
+  </WelcomeItem>
+</template>
